e76a97212d492b176a65166d306a1058e21044a9
JulioCorral <jmcorral2@miners.utep.edu>
2015-11-18 00:24:48 -0700

    in progress, demoing
    needs proper styling for all items

--- views/store/index.view.php
+++ views/store/index.view.php
@@ -7,17 +7,51 @@
 		
 
 
-		
+<table style="width:100%">
+  <tr>
+    <td>
+    	<span>UTEP El Paso Women's Watch</span>
+    	<form target="paypal" action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post">
 
-
-<form target="paypal" action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post">
 <input type="hidden" name="cmd" value="_s-xclick">
-<input type="hidden" name="hosted_button_id" value="6BE2VQB84W5EY">
+<input type="hidden" name="hosted_button_id" value="5CXVERN6PGWVC">
+<input type="image" src="https://www.sandbox.paypal.com/en_US/i/btn/btn_cart_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<img alt="" border="0" src="https://www.sandbox.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form>
+
+    </td>
+    <td>
+    	<span>UTEP 11'' x 14'' Classic Diploma Framing</span>
+    	<form target="paypal" action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="P2UAW69S2YS3Q">
+<input type="image" src="https://www.sandbox.paypal.com/en_US/i/btn/btn_cart_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<img alt="" border="0" src="https://www.sandbox.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form>
+
+    </td>		
+    <td>
+    	<span>UTEP Cap and Gown Plush Bear Alumni</span>
+    	<form target="paypal" action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="4E48QBS5VT5E6">
+<input type="image" src="https://www.sandbox.paypal.com/en_US/i/btn/btn_cart_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<img alt="" border="0" src="https://www.sandbox.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form>
+
+    </td>
+  </tr>
+  <tr>
+    <td>
+    	<span>UTEP 50th Anniversary National Basketball Champions </span>
+    	<form target="paypal" action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="HC2D2HCQEHHD2">
 <table>
-<tr><td><input type="hidden" name="on0" value="sizes">sizes</td></tr><tr><td><select name="os0">
-	<option value="small">small $599.99 USD</option>
-	<option value="medium">medium $599.99 USD</option>
-	<option value="large">large $599.99 USD</option>
+<tr><td><input type="hidden" name="on0" value="Sizes">Sizes</td></tr><tr><td><select name="os0">
+	<option value="Small">Small $56.00 USD</option>
+	<option value="Medium">Medium $56.00 USD</option>
+	<option value="Large">Large $56.00 USD</option>
 </select> </td></tr>
 </table>
 <input type="hidden" name="currency_code" value="USD">
@@ -25,27 +59,40 @@
 <img alt="" border="0" src="https://www.sandbox.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
 </form>
 
-<form target="paypal" action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post">
+    </td>
+    <td>
+    	<span>UTEP Crewneck Sweatshirt</span>
+    	<form target="paypal" action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post">
 <input type="hidden" name="cmd" value="_s-xclick">
-<input type="hidden" name="hosted_button_id" value="EP9VKXQYVDJXN">
+<input type="hidden" name="hosted_button_id" value="T6Q8DMXW2WENW">
 <table>
-<tr><td><input type="hidden" name="on0" value="Cool UTEP Mug">Cool UTEP Mug</td></tr><tr><td><input type="text" name="os0" maxlength="200"></td></tr>
+<tr><td><input type="hidden" name="on0" value="Sizes">Sizes</td></tr><tr><td><select name="os0">
+	<option value="Small">Small $56.00 USD</option>
+	<option value="Medium">Medium $56.00 USD</option>
+	<option value="Large">Large $56.00 USD</option>
+</select> </td></tr>
 </table>
+<input type="hidden" name="currency_code" value="USD">
 <input type="image" src="https://www.sandbox.paypal.com/en_US/i/btn/btn_cart_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-<img alt="" border="0" src="https://www.sandbox.paypal.com/en_US/i/scr/pixel.gif" width="10" height="10">
+<img alt="" border="0" src="https://www.sandbox.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
 </form>
 
-
-
-
+    </td>		
+    
+</table>
+		
+<br>
+<br>
+<br>
 <form target="paypal" action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post" >
-<input type="hidden" name="cmd" value="_cart">
-<input type="hidden" name="business" value="3B4GFBKFFYT5C">
-<input type="hidden" name="display" value="1">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="encrypted" value="-----BEGIN PKCS7-----MIIHBwYJKoZIhvcNAQcEoIIG+DCCBvQCAQExggE6MIIBNgIBADCBnjCBmDELMAkGA1UEBhMCVVMxEzARBgNVBAgTCkNhbGlmb3JuaWExETAPBgNVBAcTCFNhbiBKb3NlMRUwEwYDVQQKEwxQYXlQYWwsIEluYy4xFjAUBgNVBAsUDXNhbmRib3hfY2VydHMxFDASBgNVBAMUC3NhbmRib3hfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tAgEAMA0GCSqGSIb3DQEBAQUABIGAefO4bZNIyxbNzUiVstnDZbbsyhur27qISwdJfniF0VIoI1ePkPeU3XYiGbx5ojfgbdlI1flR8stmkZZkAyDs7Ua3dvPisShSjwcDL1em0+J6dFbC+SEmLS9JNKsA83uRwoCiUMxgiAsa3Qp75YOb6SnOnAaY40aJPJzlljWL2ksxCzAJBgUrDgMCGgUAMFMGCSqGSIb3DQEHATAUBggqhkiG9w0DBwQIWb8Z3wRwZ52AMN9YG0uHlC6xvxWx08hLj7qXqENRzyA5E6dTF9GPRKhHkNr7ifKx2N7wrm++5DKY1qCCA6UwggOhMIIDCqADAgECAgEAMA0GCSqGSIb3DQEBBQUAMIGYMQswCQYDVQQGEwJVUzETMBEGA1UECBMKQ2FsaWZvcm5pYTERMA8GA1UEBxMIU2FuIEpvc2UxFTATBgNVBAoTDFBheVBhbCwgSW5jLjEWMBQGA1UECxQNc2FuZGJveF9jZXJ0czEUMBIGA1UEAxQLc2FuZGJveF9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20wHhcNMDQwNDE5MDcwMjU0WhcNMzUwNDE5MDcwMjU0WjCBmDELMAkGA1UEBhMCVVMxEzARBgNVBAgTCkNhbGlmb3JuaWExETAPBgNVBAcTCFNhbiBKb3NlMRUwEwYDVQQKEwxQYXlQYWwsIEluYy4xFjAUBgNVBAsUDXNhbmRib3hfY2VydHMxFDASBgNVBAMUC3NhbmRib3hfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC3luO//Q3So3dOIEv7X4v8SOk7WN6o9okLV8OL5wLq3q1NtDnk53imhPzGNLM0flLjyId1mHQLsSp8TUw8JzZygmoJKkOrGY6s771BeyMdYCfHqxvp+gcemw+btaBDJSYOw3BNZPc4ZHf3wRGYHPNygvmjB/fMFKlE/Q2VNaic8wIDAQABo4H4MIH1MB0GA1UdDgQWBBSDLiLZqyqILWunkyzzUPHyd9Wp0jCBxQYDVR0jBIG9MIG6gBSDLiLZqyqILWunkyzzUPHyd9Wp0qGBnqSBmzCBmDELMAkGA1UEBhMCVVMxEzARBgNVBAgTCkNhbGlmb3JuaWExETAPBgNVBAcTCFNhbiBKb3NlMRUwEwYDVQQKEwxQYXlQYWwsIEluYy4xFjAUBgNVBAsUDXNhbmRib3hfY2VydHMxFDASBgNVBAMUC3NhbmRib3hfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tggEAMAwGA1UdEwQFMAMBAf8wDQYJKoZIhvcNAQEFBQADgYEAVzbzwNgZf4Zfb5Y/93B1fB+Jx/6uUb7RX0YE8llgpklDTr1b9lGRS5YVD46l3bKE+md4Z7ObDdpTbbYIat0qE6sElFFymg7cWMceZdaSqBtCoNZ0btL7+XyfVB8M+n6OlQs6tycYRRjjUiaNklPKVslDVvk8EGMaI/Q+krjxx0UxggGkMIIBoAIBATCBnjCBmDELMAkGA1UEBhMCVVMxEzARBgNVBAgTCkNhbGlmb3JuaWExETAPBgNVBAcTCFNhbiBKb3NlMRUwEwYDVQQKEwxQYXlQYWwsIEluYy4xFjAUBgNVBAsUDXNhbmRib3hfY2VydHMxFDASBgNVBAMUC3NhbmRib3hfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tAgEAMAkGBSsOAwIaBQCgXTAYBgkqhkiG9w0BCQMxCwYJKoZIhvcNAQcBMBwGCSqGSIb3DQEJBTEPFw0xNTExMTgwNzIxNTBaMCMGCSqGSIb3DQEJBDEWBBSqw/kCze6krM0vHLmzLPF1MFEq7DANBgkqhkiG9w0BAQEFAASBgCuWk9Vu2ftkIXFutN2JUxl17mX8Fg7DYo/YILitAoiWE/dZNxvQn6nEZesIT2lcMrK/cci1RXWJzYng2rbGaLY879fttyS4i1R0Janz3x+ld095XfiGS3nBpa8hCIAL8A6e2BOKmH6qq9RCa9p7G3KL33qHTHc0wM7wqErk3wkO-----END PKCS7-----
+">
 <input type="image" src="https://www.sandbox.paypal.com/en_US/i/btn/btn_viewcart_SM.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
 <img alt="" border="0" src="https://www.sandbox.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
 </form>
-		
+
+
 
 	</main>
 </div>
